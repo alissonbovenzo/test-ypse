c927395605fad293ecd33c43f86f0101549c9b17
Alisson Desandro Bovenzo <adbovenzo@gmail.com>
2017-08-25 06:17:14 -0300

    creation of module verses and completed

--- assets/app/modules/verses/index.js
+++ assets/app/modules/verses/index.js
@@ -1,8 +1,13 @@
 (function(){
     'use strict';
     
-    angular.module('ypse.verses', ['ui.router'])
-        .config(routeConfig);
+    angular.module('ypse.verses', [
+        'ui.router',
+        'ngStorage'
+    ])
+        .config(routeConfig)
+        .controller('versesListController', versesListController)
+        .controller('versesEditController', versesEditController)
 
     /** @ngInject */
     function routeConfig($stateProvider, $urlRouterProvider){
@@ -18,13 +23,71 @@
 
             .state('verses.list', {
                 url: '/list',
-                templateUrl: '/assets/app/modules/verses/templates/list.html'
+                templateUrl: '/assets/app/modules/verses/templates/list.html',
+                controller: 'versesListController'
             })
 
-            .state('verses.edit', {
-                url: '/edit',
-                templateUrl: '/assets/app/modules/verses/templates/edit.html'
+            .state('verses.new', {
+                url: '/new',
+                templateUrl: '/assets/app/modules/verses/templates/edit.html',
+                controller : 'versesEditController'
             })
+            .state('verses.edit', {
+                url: '/edit/:id',
+                templateUrl: '/assets/app/modules/verses/templates/edit.html',
+                controller : 'versesEditController'
+            })
+    }
+
+    function versesListController($scope, $localStorage){
+        $scope.verses = $localStorage.verses;
+        $scope.users = $localStorage.users;
+    }
+
+    function versesEditController($scope, $localStorage, $stateParams, $state){
+        $scope.currentPath = $state.current.name;
+        $scope.users = $localStorage.users;
+        var $verses = $localStorage.verses || [];
+
+        if(!$scope.users.length ){
+            alert('Necessário cadastrar ao menos um usuário');
+            $state.go('users.new');
+        }
+
+        function validate(text){
+            if(text.split(' ').length < 3){
+                alert('Um verso deve ter ao menos 3 palavras!');
+                return false;
+            }
+            return true;
+        }
+        
+        if($stateParams.id){
+            $scope.data = $verses[$stateParams.id]
+            $scope.submit = function(){
+                console.log($scope.data.user)
+                if(!validate($scope.data.text)) return;
+                $scope.data.timestamp = Date.now();
+                $verses[$stateParams.id] = $scope.data;
+                $localStorage.verses = $verses;
+                $state.go('verses.list');
+            }
+            $scope.delete = function(){
+                $verses.splice($stateParams.id,1);
+                $localStorage.verses = $verses;
+                $state.go('verses.list')
+            }
+            return;
+        }
+
+        $scope.submit = function(){
+            if(!validate($scope.data.text)) return;
+            $scope.data.timestamp = Date.now();
+            $verses.push($scope.data);
+            $localStorage.verses = $verses;
+            $state.go('verses.list')
+        }
+        
     }
     
 })();
